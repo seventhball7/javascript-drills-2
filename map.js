cd97e6316d1f3337eb5e30619444f6a2be8e7f01
sameer srivastava <sameersrivastava8055@gmail.com>
2021-08-16 16:37:58 +0530

    modified map

--- map.js
+++ map.js
@@ -1,7 +1,10 @@
  module.exports=map=(elements,cb)=>{
     const arr=[];
  for(let i=0;i<elements.length;i++){
+     cb(elements[i],i,elements)
+     if(cb){
      arr.push(cb(elements[i]));
+     }
  }
  return arr;
 }
